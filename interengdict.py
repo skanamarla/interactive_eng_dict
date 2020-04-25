9ef25cf324dc12983f17856b73a033735786a8b2
skanamarla <9275423+skanamarla@users.noreply.github.com>
2020-04-25 11:10:06 -0700

    Add files via upload
    added a recommendation for best match

--- interengdict.py
+++ interengdict.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -8,6 +9,10 @@ def translate(w):
     w = w.lower()
     if w in data:
         return data[w]
+
+    elif len(get_close_matches(w, data.keys())) > 0:
+        return "Did you mean %s instead?" % get_close_matches(w, data.keys())[0]
+
     else:
         return "The word doesn't exist. Please double check it."
 
